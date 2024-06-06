247911e22c462f11c764769c323c453ae948d53a
Lokusok <lokusokdrobk@gmail.com>
2024-06-06 20:25:34 +0300

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  const posts = [
+    {
+      id: 1,
+      title: 'Article 1',
+    },
+    {
+      id: 2,
+      title: 'Article 2',
+    }
+  ]
+
+  return (
+
+  );
+}
